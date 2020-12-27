ad7f9be40b63a71285ffa77e3a5e60d6a73e132f
Julia <julii.hess@gmail.com>
2020-12-27 03:39:08 +0300

    что-то поменялось

--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from tours.views import main_view, departure_view, tour_view, custom_handler404, custom_handler500
 
+from tours.views import custom_handler404
+from tours.views import custom_handler500
+from tours.views import departure_view
+from tours.views import main_view
+from tours.views import tour_view
 
 handler404 = custom_handler404
 handler500 = custom_handler500
 
 urlpatterns = [
     path('', main_view),
-    path('departure/<str:departure>', departure_view),
-    path('tour/<int:id>', tour_view),
+    path('departure/<str:departure>/', departure_view),
+    path('tour/<int:tour_id>/', tour_view),
 ]
--- tours/views.py
+++ tours/views.py
@@ -1,17 +1,73 @@
+import random
+
+from django.http import HttpResponseNotFound
+from django.http import HttpResponseServerError
 from django.shortcuts import render
-from django.http import HttpResponseNotFound, HttpResponseServerError
+
+from . import data
 
 
 def main_view(request):
-    return render(request, 'tours/index.html')
+    context = {'title': data.title,
+               'subtitle': data.subtitle,
+               'description': data.description,
+               'departures': data.departures,
+               'tours': data.tours}
+    # выбираем рандомные шесть туров
+    all_tours = []
+    for key in context['tours']:
+        all_tours.append(key)
+    random_tours = {}
+    for i in range(6):
+        random_tour = random.choice(all_tours)
+        random_tours[str(i+1)] = (context['tours'][random_tour])
+        all_tours.remove(random_tour)
+    context['tours'] = random_tours
+
+    return render(request, 'index.html', context=context)
 
 
 def departure_view(request, departure):
-    return render(request, 'tours/departure.html')
+    context = {}
+    tours = {}
+    all_tours = data.tours
+    count_of_tours = 0
+    prices_of_tours = []
+    nights = []
+    for tour, inf in all_tours.items():
+        if inf['departure'] == departure:
+            count_of_tours += 1
+            prices_of_tours.append(inf['price'])
+            nights.append(inf['nights'])
+            tours[count_of_tours] = inf
+
+    context['tours'] = tours
+    context['count_of_tours'] = count_of_tours
+    context['max_price'] = max(prices_of_tours)
+    context['min_price'] = min(prices_of_tours)
+    context['max_count_if_nights'] = max(nights)
+    context['min_count_if_nights'] = min(nights)
+    if departure == 'spb':
+        context['departure'] = 'Летим из Петербурга'
+    elif departure == 'msk':
+        context['departure'] = 'Летим из Москвы'
+    elif departure == 'kazan':
+        context['departure'] = 'Летим из Казани'
+    elif departure == 'nsk':
+        context['departure'] = 'Летим из Новосибирска'
+    elif departure == 'ekb':
+        context['departure'] = 'Летим из Екатеринбурга'
+    return render(request, 'tours/departure.html', context=context)
 
 
-def tour_view(request, id):
-    return render(request, 'tours/tour.html')
+def tour_view(request, tour_id):
+    context = {}
+    for key, value in data.tours.items():
+        if key == tour_id:
+            context = value
+    stars = '★' * int(context['stars'])
+    context['stars'] = stars
+    return render(request, 'tours/tour.html', context=context)
 
 
 def custom_handler404(request, exception):
